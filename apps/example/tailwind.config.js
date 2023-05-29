ea0d7dd34be98986cd981176073f96fd727d84b9
Thomas Misikoff <tmisikoff@gmail.com>
2023-05-28 21:08:21 -0500

    feat: upgrade to app dir and simplify

--- apps/example/tailwind.config.js
+++ apps/example/tailwind.config.js
@@ -1,9 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
